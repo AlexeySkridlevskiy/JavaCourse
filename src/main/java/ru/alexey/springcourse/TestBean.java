e48a84c6d5ade79b8a39eb286709d6798c3af591
AlexeySkridlevskiy <skrid71@gmail.com>
2022-12-31 22:41:24 +0300

    commit all files

---
+++ src/main/java/ru/alexey/springcourse/TestBean.java
@@ -0,0 +1,17 @@
+package ru.alexey.springcourse;
+
+public class TestBean {
+    private String name;
+
+    private TestBean(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
