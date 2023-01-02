37a3cb7304e406c0290cb473e6875d37c3d1892e
AlexeySkridlevskiy <skrid71@gmail.com>
2023-01-02 14:52:39 +0300

    lesson 5 HW

--- src/main/java/ru/alexey/springcourse/MusicPlayer.java
+++ src/main/java/ru/alexey/springcourse/MusicPlayer.java
@@ -1,7 +1,10 @@
 package ru.alexey.springcourse;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MusicPlayer {
-    private Music music;
+    private List<Music> musicList = new ArrayList<>();
 
     private String name;
     private int volume;
@@ -22,17 +25,13 @@ public class MusicPlayer {
         this.volume = volume;
     }
 
-    public MusicPlayer(){}
-
-    public MusicPlayer(Music music){
-        this.music = music;
+    public void setMusicList(List<Music> musicList) {
+        this.musicList = musicList;
     }
 
-    public void setMusic(Music music){
-        this.music = music;
-    }
-
-    public void playMusic(){
-        System.out.println("Playing: "+ music.getSong());
+    public void playMusicList(){
+        for(Music music: musicList){
+            System.out.println("Playing: " + music.getSong());
+        }
     }
 }
---
+++ src/main/java/ru/alexey/springcourse/RapMusic.java
@@ -0,0 +1,8 @@
+package ru.alexey.springcourse;
+
+public class RapMusic implements Music{
+    @Override
+    public String getSong() {
+        return "Snoop Dog";
+    }
+}
